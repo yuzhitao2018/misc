d7c8b44805d30ae13d42389fb405253af15f0e56
yuzhitao <yuzhitao2018@163.com>
2018-08-06 14:07:53 +0800

    modify

--- python/image_test.py
+++ python/image_test.py
@@ -6,7 +6,7 @@ import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 
 
-SIZE=250
+SIZE=10
 '''
 line_array = [ 0 for j in range(SIZE) ]
 array = [ line_array for j in range(SIZE) ]
@@ -34,8 +34,6 @@ arr.save("test.bmp", 'bmp')
 '''
 
 #创建原始图片
-image = Image.new('RGB', (SIZE, SIZE))
-draw = ImageDraw.Draw(image)
 
 def randcolor():
 	return (random.randint(0,255), random.randint(0,255), random.randint(0,255))
@@ -44,26 +42,48 @@ def randcolor_1(x, y):
 	value = int((x + y) * 0.5)
 	return (value, value, value)
 
+src_image = Image.new('RGB', (SIZE, SIZE))
+src_draw = ImageDraw.Draw(src_image)
 for x in range(SIZE):
 	for y in range(SIZE):
-		draw.point((x,y), fill=randcolor_1(x,y))
+		src_draw.point((x,y), fill=randcolor_1(x,y))
 
-#image.save("white.jpg", "jpeg")
+image_list_array = []
+#image_list_array.append(src_draw)
+
+LEVEL=1
+copy_number = 1
+fnt = ImageFont.load_default()
+
+for x in range(-1*LEVEL, LEVEL + 1, 1):
+	line_array = []
+	for y in range(-1*LEVEL, LEVEL + 1, 1):
+		if x == 0 and y == 0:
+			line_array.append(src_image)
+		else:
+			copy_image = src_image.copy()
+			copy_draw = ImageDraw.Draw(copy_image)
+			copy_draw.text((SIZE/2,SIZE/2), str(copy_number), font=fnt)
+			copy_number += 1
+			line_array.append(copy_image)
+	image_list_array.append(line_array)
+
 
 #拓展原始图片
-LEVEL=1
 NEW_SIZE = SIZE + SIZE * LEVEL * 2
 extend_image = Image.new('RGB', (NEW_SIZE, NEW_SIZE))
 
-fnt = ImageFont.load_default()
-
-number=1
-for x in range(NEW_SIZE/SIZE):
-	for y in range(NEW_SIZE/SIZE):
-		tmp_image = image.copy()
-		tmp_draw = ImageDraw.Draw(tmp_image)
-		tmp_draw.text((SIZE/2,SIZE/2), str(number), font=fnt)
-		extend_image.paste(tmp_image, box=(y * SIZE, x * SIZE, (y + 1) * SIZE, (x + 1) * SIZE))
-		number += 1
+for y in range(len(image_list_array)):
+	for x in range(len(image_list_array[y])):
+		extend_image.paste(image_list_array[y][x], box=(x * SIZE, y * SIZE, (x + 1) * SIZE, (y + 1) * SIZE))
 
 extend_image.save("extend.jpg", "jpeg")
+
+pos_array=list()
+for x in range(-1 * SIZE/2, SIZE/2 + 1):
+	pos_array.append([(y, x) for y in range(-1 * SIZE/2, SIZE/2 + 1)])
+
+for x in range(len(pos_array)):
+	print pos_array[x]
+
+		
